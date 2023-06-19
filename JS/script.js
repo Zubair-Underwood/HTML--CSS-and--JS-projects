02fbf692b7e69592f6a05b1383fbbc84c6448a51
Zubair-Underwood <finalfantasy.fantasy9@gmail.com>
2023-06-20 03:25:31 +0500

    learning

--- JS/script.js
+++ JS/script.js
@@ -1,9 +1,12 @@
 const chatInput = document.querySelector(".chat-input textarea");
 const sendChatBtn = document.querySelector(".chat-input span");
 const chatbox = document.querySelector(".chatbox");
+const chatbotToggler = document.querySelector(".chatbot-toggler");
+const chatbotCloseBtn = document.querySelector(".close-btn");
 
 let userMessage;
 const API_KEY = "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";
+const inputInitHeight = chatInput.scrollHeight;
 
 const createChatLi = (message, className) => {
     // Create a chat <li> element with passed message and className
@@ -38,6 +41,7 @@ const generateResponse = (incomingChatLi) => {
     fetch(API_URL, requestOptions).then(res => res.json()).then(data =>{
         messageElement.textContent = data.choices[0].message.content;
     }).catch((error) =>{
+        messageElement.classList.add("error");
         messageElement.textContent = "Oops! Something went wrong. Please try again.";
     }).finally(() => chatbox.scrollTo(0, chatbox.scrollHeight));
 }
@@ -45,6 +49,8 @@ const generateResponse = (incomingChatLi) => {
 const handleChat = () => {
     userMessage = chatInput.value.trim();
     if (!userMessage) return;
+    chatInput.value = "";
+    chatInput.style.height = '${inputInitHeight}px';
 
     // Append the user's message to the chatbox
     chatbox.appendChild(createChatLi(userMessage, "outgoing"));
@@ -59,4 +65,21 @@ const handleChat = () => {
     }, 600);
 }
 
+chatInput.addEventListener("input", () => {
+    // Adjust the height of the input textarea based on its content
+    chatInput.style.height = '${inputInitHeight}px';
+    chatInput.style.height = '${chatInput.scrollHeight}px';
+});
+
+chatInput.addEventListener("keydown", (e) => {
+    // If Enter key is pressed without shift key and the window
+    // width is greater than 800px, handle  the chat
+    if(e.key === "Enter" && !e.shiftKey && window.innerWidth > 800) {
+        e.preventDefault();
+        handleChat();
+    }
+});
+
 sendChatBtn.addEventListener("click", handleChat);
+chatbotCloseBtn.addEventListener("click", () => document.body.classList.remove("show-chatbot"));
+chatbotToggler.addEventListener("click", () => document.body.classList.toggle("show-chatbot"));
